f111e8056686bb4b9a685d3442e0f6aae8d5d44e
Robert A. McDougal <rmcdougal@gmail.com>
2015-08-20 21:07:09 -0400

    added condition: each color must occur an equal number of times at the end of a string

--- crane_problem.py
+++ crane_problem.py
@@ -13,31 +13,35 @@ while True:
     sets = [list(colors) for i in xrange(NUM_SETS)]
     for set in sets:
         random.shuffle(set)
-    for i in xrange(NUM_SETS - 1):
-        for j in xrange(len(colors) - PATTERN_SIZE):
-            pattern = sets[i][j : j + PATTERN_SIZE]
-            for k in xrange(i + 1, NUM_SETS):
-                repeat = False
-                for m in xrange(len(colors) - PATTERN_SIZE):
-                    pattern2 = sets[k][m : m + PATTERN_SIZE]
-                    if all(p1 == p2 for p1, p2 in zip(pattern, pattern2)):
-                        repeat = True
-                        #print '    pattern from set %d matched one in set %d' % (i, k)
-                        #print '    set %d: %r' % (i, sets[i])
-                        #print '    set %d: %r' % (k, sets[k])
+    end_colors = {c: 0 for c in colors}
+    for set in sets:
+        end_colors[set[-1]] += 1
+    if all(count == NUM_SETS / len(colors) for count in end_colors.values()):
+        for i in xrange(NUM_SETS - 1):
+            for j in xrange(len(colors) - PATTERN_SIZE):
+                pattern = sets[i][j : j + PATTERN_SIZE]
+                for k in xrange(i + 1, NUM_SETS):
+                    repeat = False
+                    for m in xrange(len(colors) - PATTERN_SIZE):
+                        pattern2 = sets[k][m : m + PATTERN_SIZE]
+                        if all(p1 == p2 for p1, p2 in zip(pattern, pattern2)):
+                            repeat = True
+                            #print '    pattern from set %d matched one in set %d' % (i, k)
+                            #print '    set %d: %r' % (i, sets[i])
+                            #print '    set %d: %r' % (k, sets[k])
+                        if repeat:
+                            break
                     if repeat:
                         break
                 if repeat:
                     break
             if repeat:
                 break
-        if repeat:
-            break
-    if not repeat:
-        successes += 1
-        print 'Found a solution! This one took %d tries to find.' % tries
-        for i, set in enumerate(sets):
-            print '    Set %d: %r' % (i + 1, set)
-        print '    (That is %d successes in %d tries so far!)' % (successes, tries)
-        print '---'
+        if not repeat:
+            successes += 1
+            print 'Found a solution! This one took %d tries to find.' % tries
+            for i, set in enumerate(sets):
+                print '    Set %d: %r' % (i + 1, set)
+            print '    (That is %d successes in %d tries so far!)' % (successes, tries)
+            print '---'
 
